6913daaa35914201f1705fbbf0ea27a1432e1d93
kogoro06 <kudo0609.study@gmail.com>
2024-10-23 01:58:20 +0900

    ログイン前ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   get "start_diagnosis", to: "diagnosis#start"
+  get 'introduce', to: 'static_pages#introduce', as: 'introduce'
 
   # Defines the root path route ("/")
   # root "posts#index"
